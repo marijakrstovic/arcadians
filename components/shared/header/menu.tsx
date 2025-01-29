08bd1000b9bd0f0987592d0a6fdd81f02feaa328
Marija <marija@fahecom.com>
2025-01-29 10:46:38 +0400

    sign out

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -8,6 +8,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
     return ( 
@@ -18,11 +19,7 @@ const Menu = () => {
                         <ShoppingCart/> Cart
                     </Link>
                 </Button>
-                <Button asChild>
-                    <Link href="/sign-in">
-                        <UserIcon/> Sign In
-                    </Link>
-                </Button>
+                <UserButton></UserButton>
             </nav>
             <nav className="md:hidden">
             <Sheet>
@@ -36,11 +33,7 @@ const Menu = () => {
                         <ShoppingCart/> Cart
                     </Link>
                     </Button>
-                    <Button asChild>
-                        <Link href="/sign-in">
-                            <UserIcon/> Sign In
-                        </Link>
-                    </Button>
+                    <UserButton></UserButton>
                     <SheetDescription></SheetDescription>
                 </SheetContent>
                 </Sheet>
